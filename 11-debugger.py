e4a93fc604652a6d63b831cfe34248061bdf0968
MarcosSouza <marcossouza.git@gmail.com>
2024-07-21 18:51:30 -0300

    Debugando o código usando f9,f5,f10 e f11

---
+++ 11-debugger.py
@@ -0,0 +1,10 @@
+# COMO DEBUGAR O CÓDIGO
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+calcular_preco_combo(30, 20)
+
+print('Programa finalizado!')
