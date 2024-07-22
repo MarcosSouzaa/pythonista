cf85dc206913076ebcc64be44f061b3e835ba432
MarcosSouza <marcossouza.git@gmail.com>
2024-07-21 22:38:31 -0300

    Operadores Lógicos

---
+++ 13-operadores_logicos.py
@@ -0,0 +1,20 @@
+# Por que queremos comparar?
+
+print('Comaração com tipos lógicos')
+idade = 15
+# Compara um valor com outro e traz uma resposta
+print(idade > 18) # False
+print(idade < 18) # True
+print(idade <= 18) # True
+print(idade >= 18) # False
+print(idade == 18) # False
+print(idade != 18) # True
+
+# Comparação entre tipos
+print('\nComparação entre tipos')
+print(True == True) # True
+print('Rafael' == 'rafael') # False
+print('b' > 'a') # True
+print(5 == '5') # False
+print(type(5)) # int
+print(type('5')) # str
